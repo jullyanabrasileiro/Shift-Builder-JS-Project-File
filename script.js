8bbcb7e6a27be66d9c1febc9bb4c796157a7ff46
Jullyana <jullyana.brasileiro@hotmail.com>
2025-02-26 18:49:54 -0800

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("Manage My Shifts is running!");
+
+//regiter page
+
+document.getElementById("registerForm").onsubmit = function (event) {
+    event.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let birthDate = document.getElementById("birthDate").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    let user = {
+        email,
+        username,
+        password,
+        firstName,
+        lastName,
+        birthDate
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+    alert("User registered successfully!");
+    window.location.href = "index.html";
+};
